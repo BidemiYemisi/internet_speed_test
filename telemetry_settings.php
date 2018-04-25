362b3a8f487a27d7afbd732d5a8f2f2c84712a39
Oyeleke <yemisi.oyeleke@bt.com>
2018-04-25 10:35:37 +0100

    added in other project files

---
+++ telemetry_settings.php
@@ -0,0 +1,20 @@
+<?php
+
+$db_type="sqlite"; //Type of db: "mysql", "sqlite" or "postgresql"
+
+// Sqlite3 settings
+$Sqlite_db_file = "..\SQLite\internet_speed.db";
+
+// Mysql settings
+$MySql_username="USERNAME";
+$MySql_password="PASSWORD";
+$MySql_hostname="DB_HOSTNAME";
+$MySql_databasename="DB_NAME";
+
+// Postgresql settings
+$PostgreSql_username="USERNAME";
+$PostgreSql_password="PASSWORD";
+$PostgreSql_hostname="DB_HOSTNAME";
+$PostgreSql_databasename="DB_NAME";
+
+?>
